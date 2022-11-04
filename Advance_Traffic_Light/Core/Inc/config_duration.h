90572ff7172d88ff599e7fc154a61a02254040d7
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-11-04 14:13:58 +0700

    add stm32 prj

---
+++ Advance_Traffic_Light/Core/Inc/config_duration.h
@@ -0,0 +1,15 @@
+/*
+ * config_duration.h
+ *
+ *  Created on: 2 thg 11, 2022
+ *      Author: Guest_demo
+ */
+
+#ifndef INC_CONFIG_DURATION_H_
+#define INC_CONFIG_DURATION_H_
+
+void change_mode_buffer(int mode);
+void inc_temp_led_duration();
+void update_led_duration(int led);
+
+#endif /* INC_CONFIG_DURATION_H_ */
